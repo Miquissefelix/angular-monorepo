34577375a363b1e2650fcc09d42def4eef9ba534
Miquissefelix <fmiquissejose@gmail.com>
2025-03-20 18:46:11 +0200

    add CI workflow file

--- apps/angular-store/src/app/app.component.ts
+++ apps/angular-store/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import{UiComponent} from '@angular-monorepo/ui';
+// import { NxWelcomeComponent } from './nx-welcome.component';
+// import{UiComponent} from '@angular-monorepo/ui';
 @Component({
-  imports: [NxWelcomeComponent, RouterModule,UiComponent],
+  imports: [ RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- libs/products/src/lib/products/products.component.ts
+++ libs/products/src/lib/products/products.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import{OrdersComponent} from '@angular-monorepo/orders';
 @Component({
   selector: 'lib-products',
   imports: [CommonModule],
